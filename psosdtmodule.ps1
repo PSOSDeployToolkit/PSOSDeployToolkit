3ba8ee29533c398581f81875b8e1985e32e0bab5
mcgbl <49388547+mcgbl@users.noreply.github.com>
2024-09-24 13:48:20 -0700

    add Get-OSDTSEnvironment Get-OSDVariable

--- psosdtmodule.ps1
+++ psosdtmodule.ps1
@@ -2,6 +2,19 @@ $PSOSDTModule = @'
 function helloOSD {
   "Hello from PSOSDTModule!"
 }
+
+function Get-OSDTSEnvironment {
+$Script:TSEnv = New-Object -ComObject Microsoft.SMS.TSEnvironment
+}
+
+function Get-OSDVariable {
+  param (
+    [Parameter(Mandatory = $True)]$Name
+  )
+  if ( ! $TSEnv ) { Get-OSDTSEnvironment }
+  $TSEnv.value($Name)
+}
+
 '@
 
 $TSEnv = New-Object -ComObject Microsoft.SMS.TSEnvironment
