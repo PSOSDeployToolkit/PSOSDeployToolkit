542fe417c16bb0f6a7ddfdc61e05aa0eead82834
mcgbl <49388547+mcgbl@users.noreply.github.com>
2024-09-24 15:20:53 -0700

    test script

---
+++ RunPowershellScriptTSStep.ps1
@@ -0,0 +1,19 @@
+"Importing Powershell OS Deployment Toolkit Module."
+$TSEnv = New-Object -ComObject Microsoft.SMS.TSEnvironment
+$PSOSDTModule = $TSEnv.value('PSOSDTModule')
+Invoke-Expression -Command $PSOSDTModule
+
+#MAIN
+
+helloOSD
+Get-OSDVariable -Name 'OSDComputerName'
+Get-OSDVariable -Name '_SMSTSPackageName'
+Set-OSDVariable -Name 'MyVar' -Value 'MyValue'
+Get-OSDVariable -Name 'MyVar'
+Get-OSDComputerSystem
+$Manufacturer
+$Model
+if ( $Manufacturer -in 'hp','hewlett packard' ) { Test-HPBIOWMIInterface }
+
+# Wait awhile to open CMTrace and view results
+Start-Sleep -Seconds 30000
